fb57f551437c555f2c8e7647f5a2962063e3d33f
Shivam Kumar <shivamkr2801@gmail.com>
2020-02-28 11:57:52 +0530

    Improvisation

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -8,16 +8,17 @@ import { Payee } from './add-payee/payee';
 export class HttpService {
 
   body: any;
+  payeeBaseUrl:string = "http://localhost:8080/payee/";
 
   constructor(private http: HttpClient) { }
 
   getPayeeByCid(cid: string) {
-    return this.http.get('http://localhost:8080/payee/' + cid)
+    return this.http.get( this.payeeBaseUrl + cid)
   }
 
   addPayee(p: Payee) {
     console.log('Inside Add payee method in Service', p);
-    return this.http.post<Payee>('http://localhost:8080/payee/', p).subscribe(data => {
+    return this.http.post<Payee>(this.payeeBaseUrl, p).subscribe(data => {
       console.log(data)
       console.log(data['name'])
       alert('Successfully added the Payee: ' + data['name'])
@@ -27,20 +28,20 @@ export class HttpService {
   }
 
   activatePayee(pid: number) {
-    return this.http.put('http://localhost:8080/payee/activate/' + pid, this.body)
+    return this.http.put(this.payeeBaseUrl+'activate/' + pid, this.body)
   }
 
   getPayeeByPID(pid) {
     console.log('Inside getPayeeByPID of Service Class' + pid)
-    return this.http.get('http://localhost:8080/payee/', { params: { 'pid': pid } })
+    return this.http.get(this.payeeBaseUrl, { params: { 'pid': pid } })
   }
 
   updatePayee(pId: number, p: Payee) {
     console.log('Inside Update payee method in Service', p);
-    return this.http.put<Payee>('http://localhost:8080/payee/' + pId, p);
+    return this.http.put<Payee>(this.payeeBaseUrl + pId, p);
   }
 
   deletePayee(pid: number) {
-    return this.http.delete('http://localhost:8080/payee/' + pid)
+    return this.http.delete(this.payeeBaseUrl + pid)
   }
 }
